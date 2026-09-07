17cc34cd4607118c879f52dd6ef9d1481d95aa40
Johan Sebastian Echeverri Ibarra <johans.echeverri@uqvirtual.edu.co>
2026-09-06 20:38:54 -0500

    Initial Commit - creación del proyecto y paquetes principales.

---
+++ src/test/java/co/uniquindio/edu/sga/SgaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
